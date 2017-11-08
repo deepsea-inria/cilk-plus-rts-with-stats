f42cc6281f221cbd08d18a7c837361e3429e0223
charguer <arthur@chargueraud.org>
2017-11-08 16:00:53 +0100

    fixe_dbuild

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-mkdir -p `pwd ../cilk-plus-rts`
+mkdir -p `pwd`/../cilk-plus-rts
 libtoolize
 aclocal
 automake --add-missing
 autoconf
-./configure --prefix=`pwd ../cilk-plus-rts`
+./configure --prefix=`pwd`/../cilk-plus-rts
 make -j
 make install
