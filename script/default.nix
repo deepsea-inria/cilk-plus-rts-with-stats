ae723bd498d9dd09dcfadefceca092d7e28d4352
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-04-06 21:44:51 -0400

    changing nix script to build by default from the local repository

--- script/default.nix
+++ script/default.nix
@@ -1,16 +1,12 @@
 { pkgs   ? import <nixpkgs> {},
   stdenv ? pkgs.stdenv,
-  fetchurl
+  cilkRtsSrc ? ../.
 }:
 
 stdenv.mkDerivation rec {
-  name = "cilk-plus-rts-with-stats-${version}";
-  version = "v1.0";
+  name = "cilk-plus-rts-with-stats";
 
-  src = fetchurl {
-    url = "https://github.com/deepsea-inria/cilk-plus-rts-with-stats/archive/${version}.tar.gz";
-    sha256 = "0c8ahsd4qk9kw6g8bqlifds664h837k1xs71nw4rwb1ysy9dccas";
-  };
+  src = cilkRtsSrc;
 
   buildInputs = [
     pkgs.libtool
